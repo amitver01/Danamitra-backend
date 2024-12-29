e01988f2b548ee11654c45b4b79a4f829e7a6cd7
Amit Verma <amitver6969@gmail.com>
2024-12-29 19:01:05 +0530

    fix 0.2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'https://danmitra.vercel.app', // replace with your frontend URL in production
+    origin: 'https://danmitra.vercel.app/', // replace with your frontend URL in production
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   };
