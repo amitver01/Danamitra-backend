15acfb49de164f12cda7fdf976783d769b9be693
Amit Verma <amitver6969@gmail.com>
2024-12-29 19:21:42 +0530

    credential for login

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(cookieParser());
 const corsOptions = {
     origin: 'https://danmitra.vercel.app', // replace with your frontend URL in production
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   };
   
 app.use(cors(corsOptions));
